096f112a623f5417f9f0c395bce4e483a40736fe
Dmitrii-Krylenko <120057236+Dmitrii-Krylenko@users.noreply.github.com>
2023-08-16 10:31:55 +0300

    место фулл правки ревью 1.1

--- backend/middlewares/errorhandler.js
+++ backend/middlewares/errorhandler.js
@@ -1,14 +1,16 @@
 const errorHandler = (err, req, res, next) => {
-  const { statusCode, message } = err;
-  if (!statusCode) {
-    return res.status(500).send({
-      message: !message
+// если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+    // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message,
     });
-  }
-
-  return next();
+  return next;
 };
 
 module.exports = errorHandler;
